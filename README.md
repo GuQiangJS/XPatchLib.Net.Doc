0cb84e74ea5fea28f7f7e35edc53fc67bdcae6ed
guqiangjs@gmail.com <gq810619>
2017-06-21 11:08:13 +0800

    update docs

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # XPatchLib.Net 帮助
 
+## 编译
+
+可以直接使用 `build.bat` 脚本进行编译。该脚本会自动根据设置编译 `XPatchLib.Net` 项目，并编译 `\src\XPatchLib.Net.Doc.sln`，最后将 .NET20 版本的帮助文档复制至 `\docs\` 目录。
+
 编译 HelperBuilder 相关帮助文档项目时需要使用 [Sandcastle Help File Builder](https://github.com/EWSoftware/SHFB/releases)。
 
 默认输出 `WebSite` 类型。如需输出 `chm` 类型则需要配合使用 [HTML Help Workshop](http://www.microsoft.com/en-us/download/details.aspx?id=21138)。
+
+## 模版
+
+`\PresentationStyles` 中包含了修改后的 `VS2013` 模版，修改内容如下：
+
+* 主要修改了 css 中的 `Font-Family` 。
+
+* 去除 `globalTemplates.xsl` 中的 `logoColumn` 。
+
+将 `\PresentationStyles` 复制至 `%ProgramFiles(x86)%\EWSoftware\Sandcastle Help File Builder` 安装目录，覆盖现有内容即可。
---
+++ build.bat
@@ -0,0 +1,73 @@
+@ECHO OFF
+
+::XPatchLib项目路径
+IF EXIST "%~dp0src\XPatchLib.Net.Doc.sln" SET "SLN=%~dp0src\XPatchLib.Net.Doc.sln"
+::XPatchLib项目路径
+IF EXIST "%~dp0..\XPatchLib.Net" SET "XPATCHLIB_SRC=%~dp0..\XPatchLib.Net"
+::XPatchLib.Net的主编译脚本，编译Release版本
+IF EXIST "%XPATCHLIB_SRC%\builder\MasterBuild.bat" SET "MASTERBUILD=%XPATCHLIB_SRC%\builder\MasterBuild.bat"
+::XPatchLib.Net Release 输出路径
+IF EXIST "%XPATCHLIB_SRC%\src\XPatchLib\bin\Release" SET "RELEASE_PATH=%XPATCHLIB_SRC%\src\XPatchLib\bin\Release\"
+IF EXIST "%~dp0..\XPatchLib.Output\Release" SET "OUTPUT_PATH=%~dp0..\XPatchLib.Output\Release\"
+
+IF NOT EXIST "%SLN%" (
+echo "%SLN% 不存在，请检查!"
+GOTO End
+)
+IF NOT EXIST "%XPATCHLIB_SRC%" (
+echo "XPatchLib项目路径 (%XPATCHLIB_SRC%) 不存在，请检查!"
+GOTO End
+)
+IF NOT EXIST "%MASTERBUILD%" (
+echo "XPatchLib.Net的主编译脚本 (%MASTERBUILD%) 不存在，请检查!"
+GOTO End
+)
+
+call %MASTERBUILD%
+
+IF NOT EXIST "%RELEASE_PATH%" (
+echo "XPatchLib.Net Release 输出路径 (%RELEASE_PATH%) 不存在，请检查!"
+GOTO End
+)
+
+xcopy /S /E /Y "%RELEASE_PATH%*.*" "%OUTPUT_PATH%"
+
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0" SET "VS150COMNTOOLS=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Community\MSBuild\15.0"
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0" SET "VS150COMNTOOLS=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0"
+IF EXIST "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0" SET "VS150COMNTOOLS=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0"
+
+IF EXIST "%VS150COMNTOOLS%\bin\MSBuild.exe" (
+SET MSBUILD="%VS150COMNTOOLS%\bin\MSBuild.exe"
+GOTO Build
+)
+
+FOR %%b in (
+	"%VS140COMNTOOLS%..\..\VC\vcvarsall.bat"
+	"%VS120COMNTOOLS%..\..\VC\vcvarsall.bat" 
+	"%VS110COMNTOOLS%..\..\VC\vcvarsall.bat"
+	"%VS90COMNTOOLS%..\..\VC\vcvarsall.bat" 
+) do (
+if exist %%b (
+	call %%b
+	SET MSBUILD="msbuild.exe"
+	goto Build
+	)
+)
+ 
+::https://social.msdn.microsoft.com/Forums/en-US/1071be0e-2a46-4c30-9546-ea9d7c4755fa/where-is-vcvarsallbat-file?forum=visualstudiogeneral
+echo "not found vcvarshall.bat"
+
+:Build
+(
+call %MSBUILD% /nologo /v:m /m %SLN% /t:Rebuild /clp:ErrorOnly;Summary;PerformanceSummary
+goto CopyResult
+)
+
+:CopyResult
+(
+IF NOT EXIST "%~dp0docs\" MD "%~dp0docs\"
+::默认复制.NET20版本的帮助文档
+xcopy /S /E /Y "%~dp0src\NET20\bin\*.*" "%~dp0docs\"
+)
+ 
+:End
